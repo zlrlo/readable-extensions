255e760a5262d154e67fee7509330a797f7d3638
zlrlo <easy10612@gmail.com>
2021-07-17 21:41:36 +0900

    ♻️ (extensions): getMetaData 함수 추가

--- apps/readable-extensions/src/content-script.js
+++ apps/readable-extensions/src/content-script.js
@@ -1,6 +1,17 @@
-const ogImage = document.querySelector('meta[property~="og:image"]');
-const ogImageContent = ogImage && ogImage.getAttribute('content');
+const getMetaData = () => {
+  const og = {};
 
-chrome.runtime.sendMessage({ greeting: 'og', ogImageContent: ogImageContent }, function (response) {
+  const image = document.querySelector('meta[property~="og:image"]');
+  const imageContent = image && image.getAttribute('content');
+  og['image'] = imageContent;
+
+  const siteName = document.querySelector('meta[property~="og:site_name"]');
+  const siteNameContent = siteName && siteName.getAttribute('content');
+  og['siteName'] = siteNameContent;
+
+  return og;
+};
+
+chrome.runtime.sendMessage({ greeting: 'og', metaData: getMetaData() }, function (response) {
   console.log(response.farewell);
 });
