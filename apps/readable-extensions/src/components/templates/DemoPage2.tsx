cb4429dff76925b062e2ce6dd3307ccbebad490d
zlrlo <easy10612@gmail.com>
2021-07-17 17:27:24 +0900

    💄 (extensions): Change to vertical

---
+++ apps/readable-extensions/src/components/templates/DemoPage2.tsx
@@ -0,0 +1,79 @@
+import { BookmarkIcon, HashtagIcon } from '@heroicons/react/solid';
+import { useEffect, useState } from 'react';
+import HashTagInput from '../modules/HashTagInput';
+
+type MetaData = {
+  image: string;
+  siteName: string;
+};
+
+const DemoPage2 = () => {
+  const defaultMetaData: MetaData = {
+    image: 'https://user-images.githubusercontent.com/68647194/125168170-056e3f80-e1df-11eb-8af8-de5395e38480.JPG',
+    siteName: 'Site name',
+  };
+
+  const [metaData, setMetaData] = useState(defaultMetaData);
+
+  const [url, setUrl] = useState('');
+
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.greeting === 'og') {
+      setUrl(sender.tab.url);
+      setMetaData(request.metaData);
+      sendResponse({ farewell: 'goodbye' });
+    }
+  });
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ['content-script.js'],
+      });
+    });
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 w-80">
+      <div className="relative z-10 col-start-1 row-start-1 px-4 pt-40 pb-3 bg-gradient-to-t from-black sm:bg-none">
+        {/* 이미지 위 글씨 영역 */}
+        <p className="text-sm font-medium text-white">READABLE</p>
+        <h2 className="text-xl font-semibold text-white">{metaData.siteName}</h2>
+      </div>
+
+      <div className="col-start-1 row-start-2 px-4">
+        {/* 이미지 아래 글씨 영역 */}
+        <div className="flex items-center text-sm font-medium my-5">
+          <BookmarkIcon className="flex-none w-5 h-5 text-blue-500"></BookmarkIcon>
+          <div className="ml-1">
+            <span className="text-black">4.9k</span>
+          </div>
+          <div className="text-base font-normal mx-2">·</div>
+          <div className="truncate">{url}</div>
+        </div>
+
+        <hr className="w-16 border-gray-300 hidden sm:block" />
+      </div>
+
+      <div className="col-start-1 row-start-3 space-y-3 px-4 pb-4">
+        {/* 이미지 아래 글씨 영역2 + 버튼 */}
+        <HashTagInput></HashTagInput>
+        <button type="button" className="bg-green-100 text-green-700 text-base font-semibold px-6 py-2 rounded-lg">
+          Save readable
+        </button>
+      </div>
+
+      <div className="relative col-start-1 row-start-1">
+        {/* 반응형 이미지 영역 */}
+        <img
+          src={metaData.image}
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover bg-gray-100 sm:rounded-lg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DemoPage2;
--- apps/readable-extensions/src/pages/index.tsx
+++ apps/readable-extensions/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import 'tailwindcss/tailwind.css';
-import DemoPage from '@extensions/src/components/templates/DemoPage';
+import DemoPage2 from '@extensions/src/components/templates/DemoPage2';
 
 // import { ReactComponent as Logo } from './logo.svg';
 // import star from './star.svg';
 
 export function App() {
-  return <DemoPage />;
+  return <DemoPage2 />;
 }
 
 export default App;
