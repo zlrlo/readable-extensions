cf143989377278da35f0b9b35e903ed1bbe65c4d
zlrlo <easy10612@gmail.com>
2021-07-17 19:50:13 +0900

    ✨ (extensions): 해시태그 추가, 삭제 기능

--- apps/readable-extensions/src/components/modules/HashTagInput.tsx
+++ apps/readable-extensions/src/components/modules/HashTagInput.tsx
@@ -1,36 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chip from '@extensions/src/components/elements/Chip';
+import { HashtagIcon, BackspaceIcon } from '@heroicons/react/solid';
+import uuid from 'react-uuid';
 
-function HashTagInput() {
+const HashTagInput = () => {
+  const [inputText, setInputText] = useState('');
+  const [hashTagList, setHashTagList] = useState([]);
+
+  const handleInputChange = e => {
+    setInputText(e.target.value);
+  };
+
+  const handleKeyPress = e => {
+    if (e.key === 'Enter') {
+      setHashTagList([...hashTagList, { id: uuid(), hashTag: inputText }]);
+      setInputText('');
+    }
+  };
+
+  const deleteHashTag = hashTagId => {
+    const filtered = hashTagList.filter(({ id }) => id !== hashTagId);
+    console.log('TCL: HashTagInput -> filtered', filtered);
+    setHashTagList(filtered);
+  };
+
   return (
     <>
-      <div className="flex">
+      <div className="flex items-center">
+        <HashtagIcon className="flex-none w-5 h-5 text-gray-500"></HashtagIcon>
         <input
-          className="border-b-2 w-full"
-          placeholder="Input..."
-          onChange={() => {
-            const test = 1;
-          }}
+          className="border-b w-full focus:ring-2 focus:ring-blue-600 p-2 rounded-md"
+          placeholder="Focus me"
+          value={inputText}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
         />
       </div>
 
-      {/* TODO(지은): ul 태그로 변경 */}
-      <div className="flex space-x-2 mb-4 text-sm font-medium">
-        <Chip backgroundColor="bg-yellow-200" fontColor="text-black">
-          #dev
-        </Chip>
-        <Chip backgroundColor="bg-purple-600" fontColor="text-white">
-          #life
-        </Chip>
-        <Chip backgroundColor="bg-green-300" fontColor="text-black">
-          #study
-        </Chip>
-        <Chip backgroundColor="bg-indigo-300" fontColor="text-black">
-          #favorites
-        </Chip>
-      </div>
+      <ul className="flex flex-wrap mb-4 text-sm font-medium">
+        {hashTagList.map(({ id, hashTag }) => {
+          return (
+            <li className="flex mr-2 mb-1">
+              <Chip key={id} backgroundColor="bg-yellow-200" fontColor="text-black">
+                #{hashTag}
+              </Chip>
+              <button
+                className="ml-1"
+                onClick={() => {
+                  deleteHashTag(id);
+                }}
+              >
+                <BackspaceIcon className="w-5 h-5 text-gray-300 hover:text-gray-500"></BackspaceIcon>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
-}
+};
 
 export default HashTagInput;
