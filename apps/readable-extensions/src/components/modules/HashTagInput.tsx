7eca8b28a96ccca41c6175345b5fce1335cef5c6
zlrlo <easy10612@gmail.com>
2021-07-18 12:59:28 +0900

    💄 (extensions): HashTagInput UI 변경
    - Input 태그 focus 되면 text color 변경

--- apps/readable-extensions/src/components/modules/HashTagInput.tsx
+++ apps/readable-extensions/src/components/modules/HashTagInput.tsx
@@ -26,10 +26,10 @@ const HashTagInput = () => {
 
   return (
     <>
-      <div className="flex items-center">
-        <HashtagIcon className="flex-none w-5 h-5 text-gray-500"></HashtagIcon>
+      <div className="flex items-center text-gray-400 focus-within:text-gray-600">
+        <HashtagIcon className="flex-none w-5 h-5"></HashtagIcon>
         <input
-          className="border-b w-full focus:ring-2 focus:ring-blue-600 p-2 rounded-md"
+          className="border-b w-full focus:outline-none p-2"
           placeholder="Focus me"
           value={inputText}
           onChange={handleInputChange}
