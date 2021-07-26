bb42c845251e476f48287c9021af21c4cdb7f6ae
tkhwang <tkhwang@gmail.com>
2021-07-27 08:16:17 +0900

    🛂 (extensions): WIP extensions - signin

--- apps/readable-extensions/src/auth.js
+++ apps/readable-extensions/src/auth.js
@@ -1,3 +1,5 @@
+// const axios = require('axios');
+
 export const authSignin = () => {
   chrome.identity.getAuthToken(
     {
@@ -14,6 +16,29 @@ export const authSignin = () => {
         .then(response => {
           console.log(response);
           alert(JSON.stringify(response));
+
+          (async () => {
+            const rawResponse = await fetch('http://localhost:7000/rest/auth/signin', {
+              method: 'POST',
+              headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ signinInput: response }),
+            });
+            const content = await rawResponse.json();
+
+            const { token } = content;
+
+            console.log(content);
+            alert(JSON.stringify(content));
+
+            if (token) {
+              chrome.storage.local.set({
+                authToken: token,
+              });
+            }
+          })();
         });
     }
   );
--- apps/readable-extensions/src/pages/login.tsx
+++ apps/readable-extensions/src/pages/login.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'tailwindcss/tailwind.css';
-import MainPage from '@extensions/src/components/templates/MainPage';
+import LoginPage from '@extensions/src/components/templates/LoginPage';
 
-const App = () => {
+const Login = () => {
   return (
     <div className="grid grid-cols-1 w-80">
-      <MainPage />
+      <LoginPage />
     </div>
   );
 };
 
-export default App;
+export default Login;
