c23648219767e676a4baae4ed8ce00d4a968f3e4
tkhwang <tkhwang@gmail.com>
2021-07-28 00:50:20 +0900

    🚧 (extensions): WIP - save bookmark

--- apps/readable-extensions/src/components/modules/Connect.tsx
+++ apps/readable-extensions/src/components/modules/Connect.tsx
@@ -2,14 +2,24 @@ import config from '@extensions/website-config';
 import React from 'react';
 
 const Connect = () => {
+  const onClick = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const url = tabs[0].url;
+
+      // TODO(Teddy): WIP
+      console.log('TCL: onClick -> url', url);
+    });
+  };
+
   return (
     <div className="flex items-end">
-      <a href={config.siteUrl} target="_blank" className="text-gray-400 hover:text-blue-600">
+      <a href={config.siteUrl} target="_blank" className="text-gray-400 hover:text-blue-600" rel="noreferrer">
         Go to the Readable
       </a>
       <button
         type="button"
         className="bg-indigo-100 text-indigo-700 text-base font-semibold px-6 py-2 rounded-lg ml-auto"
+        onClick={onClick}
       >
         Save readable
       </button>
