fae912108b72d65fa1a4151e9b61ba9005ef7c86
Teddy <tkhwang@gmail.com>
2021-07-29 07:23:18 +0900

    ♻️ (extensions): Refactor code

--- apps/readable-extensions/src/const/api.ts
+++ apps/readable-extensions/src/const/api.ts
@@ -1,5 +1,4 @@
-const SERVER_HOST = process.env.NX_SERVER_HOST;
-
+export const SERVER_HOST = process.env.NX_SERVER_HOST;
 export const API_REST_BASE = `${SERVER_HOST}/rest`;
 
 export const URL_SAVE_BOOKMARK = `${API_REST_BASE}/bookmarks/add`;
