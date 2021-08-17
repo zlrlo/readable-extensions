6cf55e1b87b56ff3be315e3b35707372e348a8b6
Teddy <tkhwang@gmail.com>
2021-08-17 19:07:05 +0900

    💄 (extensions): Add lottie animation during getting urlinfo

---
+++ apps/readable-extensions/src/components/elements/LottiePlayer.tsx
@@ -0,0 +1,18 @@
+import { Player } from '@lottiefiles/react-lottie-player';
+import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = React.PropsWithChildren<{}>;
+
+const LottiePlayer = (props: Props) => {
+  return (
+    <Player
+      autoplay
+      loop
+      src="https://assets9.lottiefiles.com/datafiles/SkdS7QDyJTKTdwA/data.json"
+      style={{ height: '200px', width: '200px' }}
+    />
+  );
+};
+
+export default LottiePlayer;
