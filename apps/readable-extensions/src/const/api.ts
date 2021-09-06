2d3c72cb41d47a5d3f274a0dfe20ab11902de9bd
Taekeun Hwang <tkhwang@gmail.com>
2021-09-06 22:34:43 +0900

    🔧 (extensions): Change local server port

--- apps/readable-extensions/src/const/api.ts
+++ apps/readable-extensions/src/const/api.ts
@@ -1,5 +1,5 @@
-export const SERVER_HOST = 'http://localhost:8000';
-// export const SERVER_HOST = 'https://readable-2021.herokuapp.com';
+// export const SERVER_HOST = 'http://localhost:8000';
+export const SERVER_HOST = 'https://readable-2021.herokuapp.com';
 
 export const API_REST_BASE = `${SERVER_HOST}/rest`;
 export const URL_SAVE_BOOKMARK = `${API_REST_BASE}/bookmarks/add`;
