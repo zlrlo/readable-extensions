ff76aace53b486381ca90a639780793a51378db4
zlrlo <easy10612@gmail.com>
2021-09-10 03:28:58 +0900

    🔧 (extensions): root 경로 변경
    - main.tsx -> index.tsx

--- apps/readable-extensions/src/index.tsx
+++ apps/readable-extensions/src/index.tsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import { RootProvider } from './store/RootProvider';
-import App from './app';
-import { SERVER_HOST } from './const/api';
+import { RootProvider } from '@extensions/store/RootProvider';
+import App from '@extensions/app';
+import { SERVER_HOST } from '@extensions/const/api';
 import 'tailwindcss/tailwind.css';
 
 console.log(`[+] 🚀 redable-extensions from foreground with ${SERVER_HOST}`);
