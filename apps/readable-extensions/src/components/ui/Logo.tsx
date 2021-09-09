d9c3d0b1df878ec3b77b0043bf47efa01513440f
zlrlo <easy10612@gmail.com>
2021-09-09 17:42:55 +0900

    ✨ (extensions): 임시 로그인 페이지 적용
    - Logo, TextLogo 컴포넌트 추가

---
+++ apps/readable-extensions/src/components/ui/Logo.tsx
@@ -0,0 +1,11 @@
+import LogoImage from '../../assets/images/logo.svg';
+
+const Logo = () => {
+  return (
+    <div className="w-11 h-11">
+      <img src={LogoImage} alt="Logo" className="object-cover" />
+    </div>
+  );
+};
+
+export default Logo;
--- apps/readable-extensions/src/pages/LoginPage.tsx
+++ apps/readable-extensions/src/pages/LoginPage.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import 'tailwindcss/tailwind.css';
+import LoginCoverImage from '../assets/images/login_cover.svg';
+import Logo from '../components/ui/Logo';
+import TextLogo from '../components/ui/TextLogo';
 import { SocialLoginButton } from '../components/ui/SocialLoginButton';
 
 const LoginPage = () => {
   return (
-    <div className="grid grid-cols-1 w-80">
-      <SocialLoginButton provider="google" />;
+    <div className="flex">
+      <div className="border-2 h-96 w-80">
+        <img src={LoginCoverImage} alt="Login" className="object-cover w-full h-full" />
+      </div>
+      <div className="flex flex-col justify-center px-7">
+        <TextLogo renderLogo={() => <Logo />} />
+
+        <div className="mt-4 text-lg font-medium text-gray-500">Read and share anything readable in ‘Readable’</div>
+
+        <div className="w-64 mt-14">
+          <SocialLoginButton provider="google" />
+        </div>
+      </div>
     </div>
   );
 };
