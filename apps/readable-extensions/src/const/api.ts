0f1f9e1cf4c762ef7b96632e3c0bb4ac492b195f
Teddy <tkhwang@gmail.com>
2021-09-28 00:50:17 +0900

    ♻️  (extensions): Remove unused environment vairable

--- apps/readable-extensions/src/const/api.ts
+++ apps/readable-extensions/src/const/api.ts
@@ -1,7 +1,4 @@
 export const SERVER_HOST = 'http://localhost:8000';
-export const SEARCH_API_BASE =
-  'https://search-readable-es-dev-662nasmydk6mmsqnizea4a4eam.ap-northeast-2.es.amazonaws.com';
-
 // export const SERVER_HOST = 'https://readable-2021.herokuapp.com';
 
 export const REST_API_BASE = `${SERVER_HOST}/rest`;
